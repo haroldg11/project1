67b47d45ca4d34fc577423e6004b265e5826d248
jussimpletrading <jussimpletrading@gmail.com>
2020-01-03 04:25:05 -0500

    intialize heroku commit

--- src/app/videolist/videolist.component.ts
+++ src/app/videolist/videolist.component.ts
@@ -5,11 +5,13 @@ import { Video } from '../video';
   selector: 'app-videolist',
   templateUrl: './videolist.component.html',
   styleUrls: ['./videolist.component.css'],
+  inputs: ['videos'],
   // tslint:disable-next-line: no-outputs-metadata-property
   outputs: ['SelectVideo']
 })
 export class VideolistComponent implements OnInit {
   public SelectVideo = new EventEmitter();
+  videos: Array<Video>;
   constructor() { }
 
   ngOnInit() {
